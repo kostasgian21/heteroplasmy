0372c01346b853537dd75cc37fed5b539b9b7b19
kostasgian21 <51513181+kostasgian21@users.noreply.github.com>
2022-04-05 10:36:10 +0200

    fixed data code

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #' A dataset containing hetoplasmy values for the HB oocyte mouse lines.
 #' Each column corresponds to a different specimen. NA values have been
 #' added to make the number of rows equal. Please remove them after loading.
-#' Heteroplasmy data of the package are in the range [0,1].
+#' Heteroplasmy data of the package are in the range \code{[0,1]}.
 #'
 #' @format A data frame with 25 rows and 56 columns (TO FIX):
 #' \describe{
@@ -18,7 +18,7 @@
 #' A dataset containing hetoplasmy values for the HB oocyte mouse lines.
 #' Each column corresponds to a different specimen. NA values have been
 #' added to make the number of rows equal. Please remove them after loading.
-#' Heteroplasmy data of the package are in the range [0,1].
+#' Heteroplasmy data of the package are in the range \code{[0,1]}.
 #'
 #' @format A data frame with 20 rows and 43 columns (TO FIX):
 #' \describe{
